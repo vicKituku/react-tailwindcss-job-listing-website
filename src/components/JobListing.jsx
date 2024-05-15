eaf1969029f9092ec6a9d8adf099a68ed881e644
VicKituku <victorkituku@gmail.com>
2024-05-15 04:57:55 +0300

    create a basic state for job listing

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 function JobListing({ job }) {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "...";
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -9,7 +16,14 @@ function JobListing({ job }) {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
+
+        <button
+          onClick={() => setShowFullDescription(!showFullDescription)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFullDescription ? "Less" : "More"}
+        </button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
