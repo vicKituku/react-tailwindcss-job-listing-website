82bca3be46bf8b55209e500f7dde2b1a04c7ba2c
VicKituku <victorkituku@gmail.com>
2024-05-17 12:12:53 +0300

    configure proxy to consume from https://fastapi-joblisting-api.onrender.com/

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://fastapi-joblisting-api.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
